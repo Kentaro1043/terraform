e46b2c94ee575bf553436695f41c1443fca06032
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-04-03 04:05:51 +0900

    ⬆️ oci moduleのアップデート

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     oci = {
-      source  = "hashicorp/oci"
-      version = "6.31.0"
+      source  = "oracle/oci"
+      version = "6.32.0"
     }
     google = {
       source  = "hashicorp/google"
