b0034ef61f556a5e32182bd255c793f551d0d812
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-04-04 06:54:15 +0900

    💥 バグってるらしいので一旦削除

--- s3.tf
+++ s3.tf
@@ -1,15 +1,15 @@
-resource "cloudflare_r2_bucket" "misskey" {
-  name          = "misskey"
-  account_id    = var.cloudflare_account_id
-  jurisdiction  = "default"
-  location      = "apac"
-  storage_class = "Standard"
-}
-
-resource "cloudflare_r2_bucket" "vaultwarden-backup" {
-  name          = "vaultwarden-backup"
-  account_id    = var.cloudflare_account_id
-  jurisdiction  = "default"
-  location      = "apac"
-  storage_class = "Standard"
-}
+#resource "cloudflare_r2_bucket" "misskey" {
+#  name          = "misskey"
+#  account_id    = var.cloudflare_account_id
+#  jurisdiction  = default
+#  location      = "apac"
+#  storage_class = "Standard"
+#}
+#
+#resource "cloudflare_r2_bucket" "vaultwarden-backup" {
+#  name          = "vaultwarden-backup"
+#  account_id    = var.cloudflare_account_id
+#  jurisdiction  = default
+#  location      = "apac"
+#  storage_class = "Standard"
+#}
