ca9971385d79ca568c4f2e7708bda6ff0a97b3e8
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-04-04 06:25:37 +0900

    ✨ r2にjurisdiction追加

--- s3.tf
+++ s3.tf
@@ -1,6 +1,7 @@
 resource "cloudflare_r2_bucket" "misskey" {
   name          = "misskey"
   account_id    = var.cloudflare_account_id
+  jurisdiction  = "default"
   location      = "apac"
   storage_class = "Standard"
 }
@@ -8,6 +9,7 @@ resource "cloudflare_r2_bucket" "misskey" {
 resource "cloudflare_r2_bucket" "vaultwarden-backup" {
   name          = "vaultwarden-backup"
   account_id    = var.cloudflare_account_id
+  jurisdiction  = "default"
   location      = "apac"
   storage_class = "Standard"
 }
